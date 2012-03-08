3cb490b121d80625ff7e301278192d1af4720ca3
Paritosh Piplewar <paritoshparitosh57@gmail.com>
2012-03-08 19:44:44 +0530

    older files removed

--- Helper/Data.php
+++
@@ -1,7 +0,0 @@
-<!-- magento/app/code/community/Lexity/Tutorial/Helper/Data.php -->
-<?php
-class Lexity_Ganalytics_Helper_Data extends Mage_Core_Helper_Abstract
-{
-
-}
-?>
--- controllers/UserControl.php
+++
@@ -1,8 +0,0 @@
-<?php 
-class Lexity_Ganalytics_UserController extends Mage_Adminhtml_Controller_Action {        
-   public function indexAction()
-    {
-        
-    }
-}
-?>
